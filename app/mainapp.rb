4b1743886b31e907a8d924760a0031070ee1641d
Tomcha <tomcha@tomcha.net>
2014-11-01 18:43:40 +0900

    Twitterでsign in、sign outの仕組みとページを作成

--- app/mainapp.rb
+++ app/mainapp.rb
@@ -3,7 +3,7 @@
 require 'sinatra/base'
 require 'haml'
 require 'omniauth'
-require 'omniauth-twitter'
+require 'omniauth-twitter' 
 require 'active_record'
 require 'sqlite3'
 require_relative 'models/oauth_user'
@@ -36,21 +36,38 @@ class MainApp < Sinatra::Base
   end
 
   get '/' do
-    puts request.object_id
+#    puts request.object_id
+    @@screen_name = 'Sign in Twitter'
+    @signin_link = '/auth/twitter'
+#    @profile_image_icon = './signin_icon.jpg'
     haml :index
   end
 
   get '/auth/twitter/callback' do
     @auth = request.env['omniauth.auth']
-    @screen_name = @auth['info'].nickname
+    session[:screen_name] = @auth['info'].nickname
+    session[:profile_image_icon] = @auth['info'].image
+    @signin_link = '#'
+    p @profile_image_icon
     session[:uid] = @auth['uid']
-    p @screen_name
-    puts "OK_OK"
     @uid = session[:uid]
-#    if !(OauthUser.find_by(twitter_id: @uid))
-#      OauthUser.create!(twitter_id: @uid, user_name: @screen_name)
-#    end
+    if !(OauthUser.find_by(twitter_id: @uid))
+      OauthUser.create!(twitter_id: @uid, user_name: @screen_name)
+    end
+    redirect '/user'
+  end
 
+  get '/user' do
+    @uid = session[:uid]
+    @screen_name = session[:screen_name]
+    @profile_image_icon = session[:profile_image_icon]
     haml :user
+  end
+
+  get '/signout' do
+    session[:uid] = nil
+    session[:screen_name] = nil
+    session[:profile_image_icon] = nil
+    redirect '/'
   end
 end
